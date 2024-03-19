1b54b7674ec123e46e489cc0cc8edba4b3e9c021
Paul Mercer <paul.mercer@qa.com>
2024-03-19 11:00:49 +0000

    Add additional nsg rule

--- nsg.tf
+++ nsg.tf
@@ -9,13 +9,25 @@ resource "azurerm_network_security_group" "example" {
   resource_group_name = azurerm_resource_group.example.name
 
   security_rule {
-    name                       = "test123"
+    name                       = "http"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "port3389"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
